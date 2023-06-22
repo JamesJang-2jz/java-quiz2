0532f504db7d34d7e7d1cffa1f27fc7eeae29fde
JamesJang-2jz <jhjx92@gmail.com>
2023-06-22 13:23:37 -0400

    finished integer array range

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -1,12 +1,20 @@
 package com.zipcodewilmington.assessment2.part1;
 
+import com.j256.ormlite.stmt.query.In;
+
+import java.util.Arrays;
+
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
         return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+        Integer[] result = new Integer[Math.abs(stop-start) + 1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = start + i;
+        } 
+        return result;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {

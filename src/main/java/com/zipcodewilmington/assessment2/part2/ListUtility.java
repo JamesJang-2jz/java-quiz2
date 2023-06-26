352efedb23e93b171c6d599f73511a459edfc7ed
JamesJang-2jz <jhjx92@gmail.com>
2023-06-26 13:19:47 -0400

    finished part2 list utility

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,29 +1,49 @@
 package com.zipcodewilmington.assessment2.part2;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListUtility {
-    public Boolean add(int i) {
-        return null;
+    List<Integer> list = new ArrayList<>();
+
+    public void add(Integer i) {
+      list.add(i);
     }
 
     public Integer size() {
-        return null;
+        return list.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+        return list.stream().distinct().collect(Collectors.toList());
     }
 
     public String join() {
-        return null;
+        String result = "";
+        for (Integer i:list) {
+            result += i;
+            result += ", ";
+        }
+        return result.substring(0, result.length()-2);
     }
 
     public Integer mostCommon() {
-        return null;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Integer result = null;
+        int maxCount = 0;
+        for (Integer i :list) {
+            int count = countMap.getOrDefault(i, 0) + 1;
+            countMap.put(i, count);
+            if (count > maxCount){
+                maxCount = count;
+                result  = i;
+            }
+        }
+        return result;
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+        return list.contains(valueToAdd);
     }
 }

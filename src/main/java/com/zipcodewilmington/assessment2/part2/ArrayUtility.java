99b50da7f2188650d94996e2fec1d6dc4a9c8801
JamesJang-2jz <jhjx92@gmail.com>
2023-06-25 23:11:08 -0400

    finished merge array pt 2

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,7 +2,10 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] totalArray = new Integer[array1.length + array2.length];
+        System.arraycopy(array1,0,totalArray,0,array1.length);
+        System.arraycopy(array2,0,totalArray,array1.length,array2.length);
+        return totalArray;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
